a6554779f6fc12787769c5f4515b6c4efd989a25
Hyde-R <hydewinter04@gmail.com>
2023-07-12 22:35:23 -0300

    GET game by id

--- src/main/java/com/maxwell04/dslist/controllers/GameController.java
+++ src/main/java/com/maxwell04/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maxwell04.dslist.dto.GameDTO;
 import com.maxwell04.dslist.dto.GameMinDTO;
 import com.maxwell04.dslist.services.GameService;
 
@@ -21,6 +23,14 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
+	//Requisição Api para encontrar um game pela ID 
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
+	
+	//Requisição Api para encontrar todos os jogos na lista
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findAll();
